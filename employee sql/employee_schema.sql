fdbaa2cad83733ca48727a8abf999c092fe1e0e9
Nashra Khan <khan.nashra3@gmail.com>
2022-05-16 19:45:54 -0400

    fourth commit

--- employee sql/employee_schema.sql
+++ employee sql/employee_schema.sql
@@ -1,49 +1,88 @@
-CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR NOT NULL
-);
+-- Remove table if it exists
+DROP TABLE IF EXISTS employees CASCADE;
 
-
-CREATE TABLE departments(
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
-);
-
-
+-- Create the table
 CREATE TABLE employees(
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
-);
-
-
--- The dept_emp table is many-many relationship, so two primary keys are needed
-CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY(emp_no, emp_title_id)
 );
 
 
+-- Remove table if it exists
+DROP TABLE IF EXISTS departments CASCADE;
+
+-- Create the table
+CREATE TABLE departments(
+	dept_no VARCHAR(5) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
+);
+
+
+
+-- Remove table if it exists
+DROP TABLE IF EXISTS departments_employees;
+
+-- Create the table
+CREATE TABLE departments_employees(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(5) NOT NULL REFERENCES departments(dept_no),
+	emp_title_id VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id),
+	PRIMARY KEY(emp_no, dept_no)
+);
+
+-- Drop the Composed Primary Key to only allow the correct number of columns
+ALTER TABLE departments_employees DROP column emp_title_id;
+
+
+-- Remove table if it exists
+DROP TABLE IF EXISTS managers;
+
+-- Create the table
+CREATE TABLE managers (
+	dept_no VARCHAR(5) NOT NULL REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id),
+	PRIMARY KEY(dept_no, emp_no)
+);
+
+-- Drop the Composed Primary Key to only allow the correct number of columns
+ALTER TABLE managers DROP column emp_title_id;
+
+
+
+-- Remove table if it exists
+DROP TABLE IF EXISTS salaries;
+
+-- Create the table
 CREATE TABLE salaries(
-	emp_no INT PRIMARY KEY,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no, emp_title_id) REFERENCES employees(emp_no, emp_title_id),
+	salary INT
 );
 
+-- Drop the Composed Primary Key to only allow the correct number of columns
+ALTER TABLE salaries DROP column emp_title_id;
 
--- The dept_manager table is many-many relationship, so two primary keys are needed
-CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL,
+-- Remove table if it exists
+DROP TABLE IF EXISTS titles;
+
+-- Create the table
+CREATE TABLE titles(
+	emp_title_id VARCHAR(5) NOT NULL,
+	title VARCHAR(30) NOT NULL,
 	emp_no INT NOT NULL,
-	PRIMARY KEY (dept_no, emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no,emp_title_id) REFERENCES employees(emp_no,emp_title_id),
+	PRIMARY KEY (emp_title_id, title)
 );
+
+-- Drop the Composed Primary Key to only allow the correct number of columns
+ALTER TABLE titles DROP column emp_no;
--- employee sql/query.sql
+++ employee sql/query.sql
@@ -1,51 +1,68 @@
--- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+-- 1. List the following details of each employee: employee number, 
+-- last name, first name, sex, and salary.
+SELECT employees.emp_no AS Employee_Number, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
-JOIN salaries ON employees.emp_no = salaries.emp_no;
+JOIN salaries ON employees.emp_no = salaries.emp_no
+LIMIT (5);
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-SELECT first_name, last_name, hire_date
+SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
-WHERE hire_date LIKE '%1986';
+WHERE employees.hire_date > '1986-1-1'::date AND employees.hire_date < '1987-1-1'::date
+LIMIT (5);
 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
-FROM dept_manager
-JOIN departments ON dept_manager.dept_no = departments.dept_no
-JOIN employees ON dept_manager.emp_no = employees.emp_no;
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+SELECT managers.dept_no, departments.dept_name, managers.emp_no, employees.last_name, employees.first_name
+FROM managers
+JOIN employees 
+ON managers.emp_no = employees.emp_no
+JOIN departments 
+ON managers.dept_no = departments.dept_no
+LIMIT (5);
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no;
+JOIN departments_employees 
+ON employees.emp_no = departments_employees.emp_no
+JOIN departments 
+ON departments_employees.dept_no = departments.dept_no
+LIMIT (5);
 
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-SELECT first_name, last_name, sex
+-- 5. List first name, last name, and sex for employees whose 
+-- first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex
 FROM employees
-WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+LIMIT (5);
 
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-SELECT * FROM departments;
--- The department number of Sales department is d007
+-- 6. List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_no = 'd007';
+JOIN departments_employees 
+ON employees.emp_no = departments_employees.emp_no
+JOIN departments 
+ON departments_employees.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales'
+LIMIT (5);
 
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT * FROM departments;
--- The department numbers of Sales and Development departments are d007 and d005
+-- 7. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
-JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
+JOIN departments_employees 
+ON employees.emp_no = departments_employees.emp_no
+JOIN departments 
+ON departments_employees.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development'
+LIMIT (5);
 
-
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name, COUNT(last_name) AS "Frequency count"
+-- 8. In descending order, list the frequency count of employee last names,
+-- i.e., how many employees share each last name.
+SELECT employees.last_name, Count (*)
 FROM employees
-GROUP BY last_name
-ORDER BY "Frequency count" DESC;
+GROUP BY employees.last_name
+ORDER BY Count (*) DESC;
